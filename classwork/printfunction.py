6b4beb1bc057022c3e2330200be3398f48564e88
manthan999 <manthanpuniwala@gmail.com>
2025-09-01 15:15:32 +0530

    print

--- classwork/printfunction.py
+++ classwork/printfunction.py
@@ -5,3 +5,14 @@ print('Hel"lo')
 print(""" hello
       word
       1234""")
+
+print("hello\\\'word")
+
+print("python","java","cpp","css",sep=" | ")
+
+print("Hello", end="")
+print("fgdfirfg")
+
+name="manthan"
+email="manthan@gmail.com"
+print(f" my {name} ,my {email}")
