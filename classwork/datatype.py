b32798bd0d6d9b7405cb88a125d67bd58223ce52
manthan999 <manthanpuniwala@gmail.com>
2025-09-03 15:39:10 +0530

    me

---
+++ classwork/datatype.py
@@ -0,0 +1,38 @@
+a= 10        #integer
+b= 20.5      #float
+c= 4+5j      #complex
+
+print(type(a))
+print(type(b))  
+print(type(c))
+print(a,b,c)
+print(a+b+c)
+
+name = "manthan"   #string
+print(type(name))
+print(name)
+
+l =(10,20,30,40)   #tuple
+print(type(l))  
+print(l)
+
+t = [10,20,30,40]   #list
+print(type(t))  
+print(t)
+
+print(range(10))   #range
+
+s = {10,20,30,40}   #set
+print(type(s))
+print(s)
+
+d = {1:"manthan",2:"kumar"}   #dictionary
+print(type(d))
+print(d)
+
+b = True    #boolean
+print(type(b))
+print(b)
+b = False
+print(type(b))
+print(b)
