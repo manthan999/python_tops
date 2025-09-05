a93b1b433a7baa23988b5cb05a2c1a55c99e8aa4
manthan999 <manthanpuniwala@gmail.com>
2025-09-05 15:06:01 +0530

    statements file

---
+++ classwork/statements.py
@@ -0,0 +1,78 @@
+#conditonal  : if-else
+#looping  :for, while
+#control : break, continue, pass
+
+
+# age = 17
+# if age>=18:
+#     print("elegeble...")
+# else:
+#     # print("not elegeble")
+#     pass
+
+# a  =1000
+# b = 200
+# c = 30
+
+# if a>b:
+#     if a>c:
+#         print("A is greater")
+#     else:
+#         print("C is greater")
+# else : 
+#     if b>c:
+#         print("B is greater")
+#     else:
+#         print("C is greater")
+
+# if a>b and a>c:
+#     print("A is greater")
+# elif b>a and b>c:
+#     print("B is greater")
+# elif c>a and c>b:
+#     print("C is greater")
+
+# marks = 90
+# 91 - 100 : a
+# 71 - 90 : b
+# 51 - 70 : c
+# 35 - 50 : d
+# 0 - 34 : F
+# other : invalid marks
+
+
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(5,10):
+#     print(i)
+
+# for i in range(1,10,2):
+#     print(i)
+
+# for i in range(10,0,-1):
+#     print(i)
+
+
+# i = 10
+# while i<20:
+#     print(i)
+#     i+=1
+
+# choice='y'
+# while choice !='n':
+#     marks = int(input("enter marks : "))
+#     if marks>=91 and marks<=100:
+#         print("A")
+#     elif marks>=71 and marks<=90:
+#         print("B")
+#     elif marks>=51 and marks<=70:
+#         print("C")
+#     elif marks>=35 and marks<=50:
+#         print("D")
+#     elif marks>=0 and marks<=34:
+#         print("F")
+#     else:
+#         print("Invalid input") 
+#     choice = input("Do u want to continue ? y or n")
