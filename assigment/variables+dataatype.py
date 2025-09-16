ccced0ab2bde32fecd0c35930f16f9bb9a0e42a5
manthan999 <manthanpuniwala@gmail.com>
2025-09-16 15:57:33 +0530

    me

---
+++ assigment/variables+dataatype.py
@@ -0,0 +1,25 @@
+Name = "manthan"                               # string
+Age = 20                                     # integer
+Hight = 5.9                                  # float
+A = 4+5j                                     # complex
+Is_Student = True                            # boolean
+Marks = [10,20,30,40]                        # list
+subjects = ( "maths","science","english")    # tuple
+Set = {10,20,30,40}                          # set
+Range = range(10)                            # range
+student_info = {
+    "name": "Rahul",
+    "age": 20,                               # dictionary
+    "grade": "B"
+}
+
+print("Name :", Name)
+print("Age :", Age)  
+print("Hight :", Hight)
+print("A :", A)
+print("Is_Student :", Is_Student)
+print("Marks :", Marks)
+print("subjects :", subjects)
+print("Set :", Set)
+print("Range :", Range)
+                         
