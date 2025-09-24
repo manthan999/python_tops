d8c71015f270146a20b2a8f4a6913e478f7151b9
manthan999 <manthanpuniwala@gmail.com>
2025-09-24 12:57:00 +0530

    fgj

---
+++ classwork/string.py
@@ -0,0 +1,36 @@
+# words = "sun rises In east"
+
+# print(len(words))
+
+# print(words.lower())
+# print(words.casefold())
+
+# print(words.upper())
+# print(words.capitalize())
+# print(words.title())
+# print(words.strip())
+
+# print(words.replace('s','K',2))
+# print(words.find("Inn"))
+
+# print(words.startswith("s"))
+# print(words.endswith("t"))
+
+# print(words.split(" "))
+
+# print("abc".join("xyz"))
+
+# print("abc".isalpha())
+# print("133q".isdigit())
+# print("sd@@@sd11".isalnum())
+
+# print("abc".zfill(10))
+# print("abc".center(11,'*'))
+
+st = "dfdf df 1211 fdsfdf434  fdf 3443"
+
+print(st)
+print(st[0:20:2])
+print(st[-4:-1])
+
+print(st[::-1])
