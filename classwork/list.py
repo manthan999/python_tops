585377c90376083c590dd8fb3c1265786dbb9e40
manthan999 <manthanpuniwala@gmail.com>
2025-09-29 12:36:45 +0530

    mr

---
+++ classwork/list.py
@@ -0,0 +1,65 @@
+# l = [10,20,30,40,50,60,50,"Hello",'a',30.66,True]
+# l1= list((10,20,30,40))
+# print(l)
+# print(len(l))
+# print(type(l))
+# print(l1)
+
+# print(l[0])
+# print(l[-1])
+# print(l[2:])
+# print(l[:5])
+# print(l[2:5])
+# print(l[2:8:2])
+# print(l[::-1])
+
+# x = ['Python','Java','Node','Php',"Android","Oracle"]
+# y  =['a','b']
+# x[1] = "SQL"
+# x.insert(1,'SQL')
+# x.append("10000")
+
+# x[2:5] = ["a","b","c","d"]
+# x.extend(y)
+# x.remove("Python")
+# x.pop(1)
+# x.clear()
+# # del x
+# print(x)
+
+
+# for i in x:
+#     print(i)
+
+# for i in range(len(x)):
+#     print(x[i])
+
+# i=0
+# while i<len(x):
+#     print(x[i])
+#     i+=1
+
+
+x = ['Python','Java',"Java",'Node','Php',"Android","Oracle"]
+
+# y = []
+
+# for i in x:
+#     if 'a' in i:
+#         y.append(i)
+
+# y = [i for i in x if 'a' in i]
+# y = ["abc" for i in x]
+# print(y)
+
+# x.sort()
+# x.sort(reverse=1)
+# x.reverse()
+
+# y = x.copy()
+# y = list(x)
+# y = x
+# print(y)
+
+print(x.count("Java"))
+print(x.index("Java"))
